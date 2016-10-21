c5c0618f731c4550c03c3f7d388782d4fc0f3801
elliotclawrence <elliotclawrence@gmail.com>
2016-10-20 21:24:46 -0700

    Updated code, wrote most of the Mem Sys

--- HW8_In_Class/HW8_In_Class/FileSystemDefs.cs
+++ HW8_In_Class/HW8_In_Class/FileSystemDefs.cs
@@ -36,7 +36,7 @@ namespace CS422
 
     public abstract class File422
     {
-        string Name { get; }
+        public string Name { get; }
 
         public Dir422 Parent { get; }
 
@@ -77,6 +77,9 @@ namespace CS422
     public class StdFSDir : Dir422
     {
         private string m_path;
+        private string dirName;
+        private Dir422 parentDir;
+        
 
         public StdFSDir(string path)
         {
@@ -87,7 +90,7 @@ namespace CS422
         {
             get
             {
-                throw new NotImplementedException();
+                return dirName;
             }
         }
 
@@ -95,13 +98,28 @@ namespace CS422
         {
             get
             {
-                throw new NotImplementedException();
+                return parentDir;
             }
         }
 
         public override bool ContainsDir(string dirName, bool recursive)
         {
-            throw new NotImplementedException();
+            foreach (string dir in Directory.GetDirectories(m_path))
+            {
+                
+
+            }
+
+            if (recursive == true) //if recursive not true, return false
+            {
+                foreach (StdFSDir child in directoryChildren) //if it is true, search children
+                {
+                    if (child.ContainsDir(dirName, true))
+                        return true;
+                }
+            }
+
+            return false;
         }
 
         public override bool ContainsFile(string fileName, bool recursive)
@@ -163,18 +181,167 @@ namespace CS422
         }
     }
 
-    public class MemoryFileSystem
-    {
 
+    public class MemoryFileSystem : FileSys422
+    {
+        MemFSDir root; 
+
+        public override Dir422 GetRoot()
+        {
+            return root;
+        }
     }
-    
-    public class MemFSDir
-    {
 
+    public class MemFSDir : Dir422
+    {
+        private string dirName;
+        private MemFSDir dirParent;
+        private List<Dir422> directoryChildren;
+        private List<File422> fileChildren;
+        private string name;
+
+        public MemFSDir(string name)
+        {
+            this.name = name;
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return dirName;
+            }
+        }
+
+        public override Dir422 Parent
+        {
+            get
+            {
+                return dirParent;
+            }
+        }
+
+        public override bool ContainsDir(string dirName, bool recursive)
+        {
+            foreach (MemFSDir child in directoryChildren) //check all the files in this folder for a match
+            {
+                if (child.Name == dirName)
+                    return true;
+            }
+            
+            if (recursive == true) //if recursive not true, return false
+            {
+                foreach (MemFSDir child in directoryChildren) //if it is true, search children
+                {
+                    if (child.ContainsDir(dirName, true))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override bool ContainsFile(string fileName, bool recursive)
+        {
+            foreach (MemFSFile child in fileChildren) //check all the files in this folder for a match
+            {
+                if (child.Name == fileName)
+                    return true;
+            }
+
+            if (recursive == true) //if recursive not true, return false
+            {
+                foreach (MemFSDir child in directoryChildren) //if it is true, search children
+                {
+                    if (child.ContainsFile(dirName, true))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override Dir422 CreateDir(string name)
+        {
+            directoryChildren.Add(new MemFSDir(name));
+            return directoryChildren[directoryChildren.Count-1];
+        }
+
+        public override File422 CreateFile(string name)
+        {
+            fileChildren.Add(new MemFSFile(name));
+            return fileChildren[fileChildren.Count - 1];
+        }
+
+        public override Dir422 getDir(string name)
+        {
+            for (int x = 0; x < directoryChildren.Count; x++)
+            {
+                if (directoryChildren[x].Name == name)
+                    return directoryChildren[x];
+            }
+
+            return null;
+        }
+
+        public override IList<Dir422> GetDirs()
+        {
+            return directoryChildren;
+        }
+
+        public override File422 GetFile(string name)
+        {
+            for (int x = 0; x < fileChildren.Count; x++)
+            {
+                if (fileChildren[x].Name == name)
+                    return fileChildren[x];
+            }
+
+            return null;
+        }
+
+        public override List<File422> GetFiles()
+        {
+            return fileChildren;
+        }
     }
 
-    public class MemFSFile
+    public class MemFSFile : File422
     {
+        private string fileName;
+        MemFSDir parentDir;
 
+        public MemFSFile(string name)
+        {
+            this.fileName = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return fileName;
+            }
+        }
+
+        public MemFSDir Parent
+        {
+            get
+            {
+                return parentDir;
+            }
+        }
+
+
+
+        public override Stream OpenReadOnly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Stream OpenReadWrite()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
